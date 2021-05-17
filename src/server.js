14e775566940cccacf7fc3c14ec4e8627361b5c3
Alois Carrera <aloiscarrera@gmail.com>
2021-05-16 22:13:05 -0500

    refactor: :pencil2: Fix typo in source folder name

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import { listenAndServe } from "https://deno.land/std/http/server.ts";
+import { acceptWebSocket, acceptable } from "https://deno.land/std/ws/mod.ts";
+import chat from "./socket/model/chat.js";
+
+listenAndServe({ port: 3000 }, (req) => {
+  if (acceptable(req)) {
+    acceptWebSocket({
+      conn: req.conn,
+      bufWriter: req.w,
+      bufReader: req.r,
+      headers: req.headers,
+    }).then(chat);
+  }
+});
+console.log("Server on port 3000");
+
+// Folder structure https://veskedevelopment.wordpress.com/2015/04/14/spring-websocket/
